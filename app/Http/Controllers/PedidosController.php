8055aabbe296b63acd564e16f051e1bc3e529c24
Vanessa <vanne.jgs@gmail.com>
2019-10-13 16:40:51 -0500

    metodos insertar pedidos no funcionales eliminados

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -10,27 +10,7 @@ use DB;
 
 class PedidosController extends Controller
 {
-    public function registrarPedido($idUsuario,$idRestaurante,$totalPedido){
-        $fecha = Carbon::now()->toDateTimeString();
-        try {
-            $pedido = Pedidos::insert(['fecha'=>$fecha,'idUsuario'=>$idUsuario,
-                    'idRestaurante'=>$idRestaurante,'totalPedido'=>$totalPedido,]);
-            if($pedido == 1){
-                $arr = array('resultado' => "insertado");
-                echo json_encode($arr);
-            } else {
-                $arr = array('resultado' => "no insertado");
-                echo json_encode($arr);
-            }              
-        } catch(\Illuminate\Database\QueryException $e){
-            $errorCode = $e->getMessage();
-            $arr = array('estado' => $errorCode);
-
-            echo json_encode($arr);
-        }
-    }
-
-
+    
     public function insertarPedido($idUsuario,$idRest,$idProd,$precio,$idPago,$especs,$cantidad,$totalPedido,$ubicacion){
         $fecha = Carbon::now()->toDateTimeString();
         try {
@@ -52,28 +32,6 @@ class PedidosController extends Controller
                     $arr = array('resultado' => "no insertado");
                     echo json_encode($arr);
                 }     
-        } catch(\Illuminate\Database\QueryException $e){
-            $errorCode = $e->getMessage();
-            $arr = array('estado' => $errorCode);
-
-            echo json_encode($arr);
-        }
-    }
-
-
-    //http://127.0.0.1:8000/insertarPedido/idUsuario/idRest/idProd/precio/idPago(1)/Especs:ninguna/totalComp/ubicacion/cantidad
-    public function insertarPedido2($idUsuario,$idRest,$idProd,$precio,$idPago,$especs,$totalPedido,$ubicacion,$cantidad){
-        $fecha = Carbon::now()->toDateTimeString();
-        try {
-            $pedido = Pedidos::insert(['fecha'=>$fecha,'idUsuario'=>$idUsuario,
-                    'idRestaurante'=>$idRest,'totalPedido'=>$totalPedido,]);
-            if($pedido == 1){
-                $arr = array('resultado' => "insertado");
-                echo json_encode($arr);
-            } else {
-                $arr = array('resultado' => "no insertado");
-                echo json_encode($arr);
-            }              
         } catch(\Illuminate\Database\QueryException $e){
             $errorCode = $e->getMessage();
             $arr = array('estado' => $errorCode);
