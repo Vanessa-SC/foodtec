6c3a8ab513508adde0196bcbfa09106abc1d91a6
Mayri Bautista <16041328@itdurango.edu.mx>
2019-10-16 20:04:59 -0500

    j

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('insertarPedido/{idUsuario}/{idRest}/{idProd}/{precio}/{idPago}/{espe
 Route::get('insertarPedido/{idPago}/{idUsuario}/{idRestaurante}/{totalPedido}',['uses' => 'PedidosController@insertarPedido']);
 Route::get('consultarPedidoActual/{idUsuario}',['uses' => 'PedidosController@consultarPedidoActual']);
 Route::get('registrarPedido/{idUsuario}/{idRestaurante}/{totalPedido}',['uses' => 'PedidosController@registrarPedido']);
-Route::get('mostrarPedido', ['uses' => 'PedidosController@mostrarPedidos']);
+Route::get('mostrarPedidos/', ['uses' => 'PedidosController@mostrarPedidos']);
 
     //RUTAS DE PRUEBA
 Route::get('comprobarCorreo/{correo}', ['uses' => 'UsuariosController@comprobarCorreo']);
